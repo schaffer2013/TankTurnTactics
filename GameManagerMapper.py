482fa488ec0bb386cfd4aef7eb01d1e1ce99aacb
Phil Schaffer <phil.schaffer@dmcinfo.com>
2023-05-06 14:35:57 -0700

    Renamed and expanded mapper capability

--- GameManagerMapper.py
+++ GameManagerMapper.py
@@ -1,13 +1,26 @@
 import pygame
 
 # Game Helper
-ARROW_KEYS=[pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN]
+ARROW_KEYS = [pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN]
 
-class KeyboardMapper:
-    def __init__(self, manager):
+
+class OmnipotentMapper:
+    def __init__(self, manager, screenWidth, screenHeight, screenMargin):
         self.gameManager = manager
         self.isActiveArmed = False
         self.isActiveReadyToGive = False
+        self.WIDTH = screenWidth
+        self.HEIGHT = screenHeight
+        self.MARGIN = screenMargin
+
+    def mapMouseEvent(self, event):
+        # User clicks the mouse. Get the position
+        pos = pygame.mouse.get_pos()
+        # Change the x/y screen coordinates to grid coordinates
+        column = pos[0] // (self.WIDTH + self.MARGIN)
+        row = pos[1] // (self.HEIGHT + self.MARGIN)
+        if self.isActiveArmed:
+            self.gameManager.tryShoot((column, row))
 
     def mapKeyboardEvent(self, event):
         if (event.type == pygame.KEYDOWN and event.key == pygame.K_p):
@@ -16,9 +29,9 @@ class KeyboardMapper:
             success = self.tryActionActiveTank(event)
             if (event.type == pygame.KEYDOWN and not success):
                 print("Unavailable action")
-    
+
     def tryActionActiveTank(self, event):
-        success = False 
+        success = False
         if (event.type == pygame.KEYDOWN):
             if (not self.gameManager.getActiveTankHasActionPoints()):
                 return False
